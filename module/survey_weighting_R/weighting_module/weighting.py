705474640cd79f77cca8943e27c88eb97aaffb7c
Safwan <safwanshamsir7177@gmail.com>
2024-06-27 21:35:25 +0800

    fix: request to endpoint using request body

--- module/survey_weighting_R/weighting_module/weighting.py
+++ module/survey_weighting_R/weighting_module/weighting.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import pandas as pd
 import requests
-from module.survey_weighting_R.utils_module.utils import convert_list_to_dict, convert_df_to_json
+from module.survey_weighting_R.utils_module.utils import (
+    convert_list_to_dict, 
+    convert_df_to_json,
+    convert_dict_to_json
+    )
 
 url = st.secrets["survey_weighting"]["ENDPOINT"]
 
@@ -12,24 +16,40 @@ def calc_weight(
         lower_limit:int, 
         upper_limit:int):
     '''
-    
+    Backend function to calculate the weight of cleaned survey responses.
+    This script serves as the top script for the back-end of the survey weighting R.
+
+    Args:
+        - df: pandas DataFrame 
+        - sample_list: List of name of the selected demography columns. 
+        - pop_list: List of pandas DataFrame. 
+        - lower_limit: lower limit for the trimmed_weight column.
+        - upper_limit: upper limit for the trimmed_weight column.
+
+    Return:
+        - response.json: TBD
     '''
     survey_data = convert_df_to_json(df=df)
     pop_dict = convert_list_to_dict(pop_list=pop_list)
-    query_params = {
-        "sample_list_json": ",".join(sample_list),
+    sample_dict = {
+        "sample_list": sample_list
+        }
+    sample_list_json = convert_dict_to_json(sample_dict)
+    limit_dict = {
         "lower_limit": lower_limit,
         "upper_limit": upper_limit
     }
-    # response = requests.post(
-    #     url=url,
-    #     params=query_params,
-    #     json={
-    #         "survey_file": survey_data,
-    #         "pop_list_json": pop_dict
-    #     }
-    # )
-    response  = requests.get(url=url)
+    limit = convert_dict_to_json(limit_dict)
+    response = requests.post(
+        url=url,
+        json={
+            "survey_file": survey_data,
+            "sample_list_json": sample_list_json,
+            "pop_list_json": pop_dict,
+            "limit": limit
+        }
+    )
+
     if response.status_code == 200:
         return response.json()
     else:
